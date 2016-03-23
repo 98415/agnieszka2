e420371c15fa551fcb52007f1966b61c3dfd1d40
Agnieszka Kowalczyk <36785@student.ue.poznan.pl>
2016-03-23 12:57:51 +0100

    dodalem funkcje get_info

--- plik.py
+++ plik.py
@@ -2,5 +2,10 @@ def dodawanie():
  a = int(input("podaj a"))
  b = int(input("podaj b"))
  print(a+b)
+ 
+def get_info():
+  return "To jest program kalkulator stworzony przez Gunie"
+
+print(get_info())
 dodawanie()
 input()
